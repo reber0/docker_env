5c1e3ea86e8602d9d3813e65509fef5696bd9709
reber <reber0@163.com>
2019-02-26 11:26:40 +0800

    Update index.php

--- apache/web/index.php
+++ apache/web/index.php
@@ -1,8 +1,24 @@
 <?php
-    $link = mysql_connect("mysql", "root", "root");
-    if(!$link)
+    $conn = mysql_connect("mysql", "root", "root");
+    if(!$conn)
         die('Could not connect: ' . mysql_error());
-    else
-        echo "Connection mysql successful";
-    mysql_close($link);
+    mysql_select_db('rtest',$conn);
+
+    if (isset($_GET['id'])) {
+        @$id = $_GET['id'];
+        $result=mysql_query("select * from msg where id=".$id);
+        $rows = @mysql_fetch_assoc($result);
+
+        if ($rows) {
+            foreach ($rows as $key => $value) {
+                echo $key.'----'.$value.'<br/>';
+            }
+        } else {
+            echo mysql_error();
+        }
+    } else {
+        echo "please input id";
+    }
+
+    mysql_close($conn);
 ?>
