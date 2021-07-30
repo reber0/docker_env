e63f12dc29eecba4f6a67661e627250e10658b3e
reber <reber0ask@qq.com>
2021-07-30 23:19:38 +0800

    update

--- apache/web/index.php
+++ apache/web/index.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("mysql5.5", "root", "root", "rtest");
+    $conn = mysqli_connect("mysql5.5", "root", "root", "test_db");
     if(!$conn)
         die('Could not connect: ' . mysqli_error($conn));
 
